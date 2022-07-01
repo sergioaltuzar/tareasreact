70842e05d87c1258416eba5cfe31f773273ca7e8
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-06-30 21:07:18 -0500

    Update Tarea.js

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -1,15 +1,15 @@
 import React from "react";
 import '../hojas-de-estilo/tarea.css';
+import {AiOutlineCloseCircle} from 'react-icons/ai';
 
-
-function Tarea({ texto }) {
+function Tarea({ texto, completada }) {
     return (
-        <div className='tarea-contenedor'>
+        <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
             <div className='tarea-texto'>
                 {texto}
             </div>  
-            <div className='tarea-icono'>
-                Eliminar            
+            <div className='tarea-contenedor-iconos'>
+                <AiOutlineCloseCircle className='tarea-icono' />            
             </div>  
         </div>    
     );
