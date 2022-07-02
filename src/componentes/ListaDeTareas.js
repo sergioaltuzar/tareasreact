bd3cb9da0a2388b52450a825a3133cfb8bc91fcb
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-07-02 15:45:17 -0500

    key id

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -6,17 +6,22 @@ import Tarea from './Tarea';
 function ListaDeTareas() {
     const [tareas, setTareas] = useState([]);
     const agregarTarea = tarea => {
-        console.log('tarea agreada');
-        console.log(tarea);
+        if (tarea.texto.trim()) {
+            tarea.texto = tarea.texto.trim();
+            const tareasActualizadas = [tarea, ...tareas];
+            setTareas(tareasActualizadas);
+        }
     }
 
   return (
     <>
-        <TareaFormulario/>
+        <TareaFormulario onSubmit={agregarTarea} />
         <div className='tareas-lista-contenedor'>
             {
                 tareas.map((tarea) => 
                     <Tarea 
+                        key={tarea.id}
+                        id={tarea.id}
                         texto={tarea.texto}
                         completada={tarea.completada}
                     />
--- src/componentes/TareaFormulario.js
+++ src/componentes/TareaFormulario.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import '../hojas-de-estilo/TareaFormulario.css';
+import {v4 as uuidv4} from 'uuid';
 
 function TareaFormulario (props) {
 
@@ -14,10 +15,11 @@ function TareaFormulario (props) {
         e.preventDefault();
         
         const tareaNueva = {
-            id: ,
+            id: uuidv4(),
             texto: input,
             completada: false
         }
+        props.onSubmit(tareaNueva);
     }
 
     return (
